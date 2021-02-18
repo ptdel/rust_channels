5a05b742d4cbae2c57af829a743ca040bd44a473
Patrick Delaney <pthomasdelaney@gmail.com>
2021-02-17 19:28:39 -0800

    chore: adding delete and post routes

--- rch/src/routes.rs
+++ rch/src/routes.rs
@@ -1,24 +1,38 @@
-use rchdb::{list,get,delete};
+use rocket::response::status::{Created,NoContent};
+use diesel::result::Error;
+use rchdb::{list,get,new,delete};
 use rchdb::models::{Thread};
-use rocket_contrib::json::Json;
-use rocket::response::status::NotFound;
+use rocket_contrib::json::{Json};
 use crate::DbConn;
 
 
 #[get("/")]
-pub fn list_threads(conn: DbConn) -> Result<Json<Vec<Thread>>, NotFound<String>> {
-    list(&conn).map(|thread| Json(thread)).map_err(|e| NotFound(e.to_string()))
+pub fn list_threads(conn: DbConn) -> Result<Json<Vec<Thread>>, Error> {
+    list(&conn)
+        .map(|thread| Json(thread))
+        .map_err(|err| err)
 }
 
 #[get("/<id>")]
-pub fn get_thread(id: i32, conn: DbConn) -> Result<Json<Thread>, NotFound<String>> {
-    get(id, &conn).map(|thread| Json(thread)).map_err(|e| NotFound(e.to_string()))
+pub fn get_thread(id: i32, conn: DbConn) -> Result<Json<Thread>, Error> {
+    get(id, &conn)
+        .map(|thread| Json(thread))
+        .map_err(|err| err)
+}
+
+#[post("/", format = "application/json", data = "<thread>")]
+pub fn new_thread(thread: Json<Thread>, conn: DbConn) -> Result<Created<Json<Thread>>, Error> {
+    new(thread.into_inner(), &conn)
+        .map(|person|
+            Created(
+                format!("/people/{id}", id = person.id),
+                Some(Json(person))))
+        .map_err(|err| err)
 }
 
 #[delete("/<id>")]
-pub fn delete_thread(id: i32, conn: DbConn) -> Result<String, NotFound<String>> {
-    match delete(id, &conn) {
-        Ok(_) => format!("{{ \"deleted\": \"{}\" }}", id),
-        Err(e) => NotFound(e.to_string()),
-    }
+pub fn delete_thread(id: i32, conn: DbConn) -> Result<NoContent, Error> {
+    delete(id, &conn)
+        .map(|_| NoContent)
+        .map_err(|err| err)
 }
